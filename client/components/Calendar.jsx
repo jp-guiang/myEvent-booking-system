a5f0847c46d5a67de2a0f64ae92cca258bb9cf51
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-09-04 15:12:51 +1200

    test event

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
 
 export default function Calendar() {
+  const headerObj = {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'timeGridDay,timeGridWeek,dayGridMonth',
+  }
+  const dateObj = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }
+  //year-month-day
+
+  // Use this function toconvert date to ISO
+  // var date = new Date();
+  // date.toISOString();
+  const myEvent = [
+    {
+      title: 'Pepe please work',
+      start: '2022-09-04T12:30:00',
+      end: '2022-09-04T14:30:00',
+    },
+  ]
+
   return (
     <>
       <h1>Pepe Test</h1>
       <h2>Why you like this jp</h2>
       <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        events={[{ title: 'Pepe please work', date: new Date(Date.now()) }]}
+        plugins={[timeGridPlugin, dayGridPlugin]}
+        headerToolbar={headerObj}
+        titleFormat={dateObj}
+        timeZone="New_Zealand/Wellington"
+        initialView="timeGridWeek"
+        events={myEvent}
       />
     </>
   )
